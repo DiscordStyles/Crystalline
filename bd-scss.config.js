4429261f7da4ae69c8fb8fc1692422dc048263db
Ian <Gibbu_@hotmail.com>
2022-07-04 16:48:39 +1000

    v0.0.6

--- bd-scss.config.js
+++ bd-scss.config.js
@@ -3,7 +3,7 @@ export default {
 	meta: {
 		name: "Crystalline",
 		author: "Gibbu",
-		version: "0.0.5",
+		version: "0.0.6",
 		description: "A beautiful frosted glass theme with support for both dark and light themes.",
 		source: "https://github.com/DiscordStyles/Crystalline",
 		authorId: '174868361040232448'
